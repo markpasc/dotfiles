5bd4b0adc4d13d2d17f3bc9ef217f2e24f452f29
Mark Paschal <markpasc@markpasc.org>
2009-10-10 00:33:11 -0700

    Include dw_orange vim colors file

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-FILES=".bashrc .colordiffrc .screenrc .vimrc bin/*"
+FILES=".bashrc .colordiffrc .screenrc .vimrc .vim/colors/dw_orange.vim bin/*"
 
 cd ~/.dotfiles
 for file in $FILES
 do
+    DIR=`dirname ../$file` test -d $DIR || mkdir $DIR
     ln -sniv ~/.dotfiles/$file ../$file
 done
