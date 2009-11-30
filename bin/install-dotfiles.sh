ad0e59afc19e64dac64562033acefdf516a2e7b7
Mark Paschal <markpasc@markpasc.org>
2009-11-30 10:44:02 -0800

    Configure a global gitignore file when installing, since we can't specify a platform-neutral home directory directly in .gitconfig

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILES=".bashrc .bash_aliases .colordiffrc .gitconfig .screenrc .vimrc .vim/colors/dw_orange.vim bin/*"
+FILES=".bashrc .bash_aliases .colordiffrc .gitconfig .gitignore-global .screenrc .vimrc .vim/colors/dw_orange.vim bin/*"
 
 cd ~/.dotfiles
 for file in $FILES
@@ -8,3 +8,7 @@ do
     DIR=`dirname ../$file` test -d $DIR || mkdir $DIR
     ln -snfv ~/.dotfiles/$file ../$file
 done
+
+# fix up git config to use ignores file
+git config --global core.excludesfile ~/.gitignore-global
+
