87e0aeeb5552175dda40bb683955aa4780b1575e
Mark Paschal <markpasc@markpasc.org>
2010-09-10 10:55:37 -0700

    Configure logging too when starting up the python shell

--- bin/pyhistory.py
+++ bin/pyhistory.py
@@ -1,9 +1,21 @@
-import os, readline
-histfile = os.path.join(os.environ["HOME"], ".python_history")
-try:
-    readline.read_history_file(histfile)
-except IOError:
-    pass
-import atexit
-atexit.register(readline.write_history_file, histfile)
-del os, histfile, readline
+def set_up_history():
+    import os, readline, atexit
+
+    histfile = os.path.join(os.environ["HOME"], ".python_history")
+    try:
+        readline.read_history_file(histfile)
+    except IOError:
+        pass
+
+    atexit.register(readline.write_history_file, histfile)
+
+def set_up_logging():
+    import logging
+    logging.basicConfig(level=logging.DEBUG,
+        format='%(asctime)-8s %(levelname)-8s %(name)-15s %(msg)s',
+        datefmt='%H:%M:%S')
+
+set_up_history()
+set_up_logging()
+
+del set_up_history, set_up_logging
