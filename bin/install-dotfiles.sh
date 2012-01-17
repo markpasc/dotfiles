c6c8647bc519a94d8424c2d15745fb071ecd2d22
Mark Paschal <markpasc@markpasc.org>
2012-01-16 16:51:24 -0800

    Guess git supports homedir in the excludesfile now, so just put it in instead of always locally modifying

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -5,12 +5,9 @@ FILES=".bashrc .bash_mac .bash_aliases .colordiffrc .gitconfig .gitignore-global
 cd ~/.dotfiles
 for file in $FILES
 do
-    DIR=`dirname ../$file` test -d $DIR || mkdir $DIR
+    DIR=`dirname ../$file` test -d $DIR || mkdir -p $DIR
     ln -snfv ~/.dotfiles/$file ../$file
 done
-
-# fix up git config to use ignores file
-git config --global core.excludesfile ~/.gitignore-global
 
 # run the .osx stuff
 sh ~/.dotfiles/mac
