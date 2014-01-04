e842a841674925890ff47eb693ac294163b300b6
Mark Paschal <markpasc@markpasc.org>
2014-01-04 01:40:41 -0500

    Store symlinked dotfiles without dots so I can actually edit them

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-FILES=".bashrc .bash_mac .bash_aliases .colordiffrc .gitconfig .gitignore-global .hgrc .screenrc .vimrc .vim/colors/dw_orange.vim bin/*"
+DOTFILES="bashrc bash_mac bash_aliases colordiffrc gitconfig gitignore-global hgrc screenrc vimrc vim/colors/dw_orange.vim"
+FILES="bin/*"
 
 cd ~/.dotfiles
+for file in $DOTFILES
+do
+    DIR=`dirname ../.$file` test -d $DIR || mkdir -p $DIR
+    ln -snfv ~/.dotfiles/$file ../.$file
+done
+
 for file in $FILES
 do
-    DIR=`dirname ../$file` test -d $DIR || mkdir -p $DIR
-    ln -snfv ~/.dotfiles/$file ../$file
+	DIR=`dirname ../$file` test -d $DIR || mkdir -p $DIR
+	ln -snfv ~/.dotfiles/$file ../$file
 done
 
 # Include pywhich from its submodule.
@@ -15,5 +22,5 @@ ln -snfv ~/.dotfiles/pywhich/bin/pywhich ../bin/pywhich
 # run the .osx stuff on the mac
 if [ "x$SYSTEM" == "xDarwin" ]
 then
-    sh ~/.dotfiles/mac
+    sh ~/.dotfiles/mac_init.sh
 fi
